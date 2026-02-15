81c45071701c6bd181a682848de7df9f4877e17e
ParasOli <parasoli7379@gmail.com>
2026-02-15 23:10:41 +0545

    Allure report implementation

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com",
     setupNodeEvents(on, config) {
+      // Allure must be first
+      allureWriter(on, config);
+      return config;
+    },
+    env: {
+      allure: true
     },
   },
 });
