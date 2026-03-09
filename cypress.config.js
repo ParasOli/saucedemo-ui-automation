840cd877d873cde56b2bce82e5e96a05ff6ba45f
ParasOli <parasoli7379@gmail.com>
2026-03-09 22:27:46 +0545

    change in slack message format

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,16 @@ const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
+  projectId: "vu7m84",
   e2e: {
+    experimentalPromptCommand : true,
+    experimentalModifyObstructiveThirdPartyCode: true,
     baseUrl: "https://www.saucedemo.com",
     setupNodeEvents(on, config) {
-      // Allure must be first
       allureWriter(on, config);
       return config;
     },
+
     env: {
       allure: true
     },
--- cypress/e2e/regression/login.cy.js
+++ cypress/e2e/regression/login.cy.js
@@ -7,34 +7,34 @@ describe("SauceDemo Login Tests", () => {
     LoginPage.visit('/');
   });
 
-  it("1️⃣ Login with valid credentials", () => {
+  it("Login with valid credentials", () => {
     LoginPage.login("standard_user", "secret_sauce");
     cy.url().should("include", "/inventory.html");
   });
 
-  it("2️⃣ Login with invalid username", () => {
+  it("Login with invalid username", () => {
     LoginPage.login("wrong_user", "secret_sauce");
     LoginPage.checkErrorMessage(
       "Epic sadface: Username and password do not match any user in this service"
     );
   });
 
-  it("3️⃣ Login with invalid password", () => {
+  it("Login with invalid password", () => {
     LoginPage.login("standard_user", "wrong_password");
     LoginPage.checkErrorMessage(
       "Epic sadface: Username and password do not match any user in this service"
     );
   });
 
-  it("4️⃣ Login with locked out user", () => {
+  it("Login with locked out user", () => {
     LoginPage.login("locked_out_user", "secret_sauce");
     LoginPage.checkErrorMessage(
       "Epic sadface: Sorry, this user has been locked out."
     );
   });
 
-  it("5️⃣ Login with empty username and password", () => {
-    LoginPage.login("", "");
+  it("Login with empty username and password", () => {
+    cy.get('#login-button').click()
     LoginPage.checkErrorMessage(
       "Epic sadface: Username is required"
     );
